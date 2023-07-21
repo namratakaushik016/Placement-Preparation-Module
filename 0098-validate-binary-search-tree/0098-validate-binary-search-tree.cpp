461aa6d600f7f1f3c4bdfe2bdc760e9f706efc78
namratakaushik016 <118747968+namratakaushik016@users.noreply.github.com>
2023-07-21 12:27:41 +0530

    Time: 11 ms (83.35%), Space: 21.7 MB (57.49%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -22,10 +22,12 @@ public:
         
         if (node->val <= minVal || node->val >= maxVal) {
             return false;
+            //we check whether the value of the current node (node->val) is less than or equal to the minVal or greater than or equal to the maxVal. If either of these conditions is true, it means the current node's value violates the BST property, and we return false to indicate that this subtree is not a valid BST.
         }
         
         return isValidBSTHelper(node->left, minVal, node->val) &&
                isValidBSTHelper(node->right, node->val, maxVal);
+        //The idea behind these checks is that the left subtree of a node must contain only nodes with keys less than the node's key, and the right subtree must contain only nodes with keys greater than the node's key.
     }
 };
     
